a197a8ceae91dbd9c4cb97e3c629943743b79f45
Ketandabhi18 <fs1.webinent@gmail.com>
2024-02-29 16:50:31 +0530

    cloudinary and multer setup

---
+++ src/middleware/upload.ts
@@ -0,0 +1,62 @@
+import { v2 as cloudinary } from "cloudinary";
+import { unlinkSync } from "fs";
+
+const upload = async (localFilePath: string) => {
+    try {
+        console.log("upload :: localFilePath :: ", localFilePath);
+
+        // const cloudinaryResponse = await cloudinary.uploader.upload(localFilePath);
+        const cloudinaryResponse = await cloudinary.uploader.upload(localFilePath, { resource_type: "auto" });
+        // console.log("upload :: coludinaryResponse :: ", cloudinaryResponse);
+        const { secure_url } = cloudinaryResponse;
+        if (!secure_url) {
+            unlinkSync(localFilePath);
+            return {
+                isSuccess: false,
+                statusCode: 400,
+                data: {},
+                message: "Couldn't upload your image at the moment. Please try again later."
+            };
+        }
+
+        // unlinkSync(localFilePath);
+        return {
+            isSuccess: true,
+            statusCode: 200,
+            data: cloudinaryResponse,
+            message: "Successfully uploaded image.",
+        };
+    } catch (error) {
+        console.log("upload :: error :: ", error);
+        return {
+            statusCode: 500,
+            isSuccess: false,
+            data: error,
+            message: "Something went wrong."
+        };
+    }
+}
+
+const removeImageCloudinary = async (public_id: string) => {
+    try {
+        let remove = await cloudinary.uploader.destroy(public_id);
+        console.log("removeImageCloudinary :: remove :: ", remove);
+
+        return {
+            statusCode: 200,
+            isSuccess: true,
+            data: remove,
+            message: "Remove Image"
+        }
+    } catch (error) {
+        console.log("removeImageCloudinary :: error :: ", error);
+        return {
+            statusCode: 500,
+            isSuccess: false,
+            data: error,
+            message: "Something went wrong."
+        };
+    }
+}
+
+export { upload, removeImageCloudinary };

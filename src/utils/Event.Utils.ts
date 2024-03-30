2ca6699a04e6aafdccbabd06ed32c27d7aa7ed46
Ketandabhi18 <fs1.webinent@gmail.com>
2024-03-30 18:36:05 +0530

    changes in event apis

--- src/utils/Event.Utils.ts
+++ src/utils/Event.Utils.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { Messages, generalMessage, statusCode } from "../config/constant";
+const moment = require("moment-timezone");
 
 export const getActiveEventsUtils = async () => {
   try {
@@ -23,21 +24,47 @@ export const getActiveEventsUtils = async () => {
 
 export const registerForEventUtils = async (data: any, headers: any) => {
   try {
+    const inputDateTimeArrival = moment.tz(
+      data.arrivalDate,
+      "DD-MM-YYYY HH:mm",
+      "Asia/Kolkata"
+    );
+
+    const inputDateTimeDeparture = moment.tz(
+      data.departureDate,
+      "DD-MM-YYYY HH:mm",
+      "Asia/Kolkata"
+    );
+    const outputDateTimeArrival =
+      inputDateTimeArrival.format("YYYY-MM-DD HH:mm");
+    const outputDateTimeDeparture =
+      inputDateTimeDeparture.format("YYYY-MM-DD HH:mm");
+
+    console.log(`${outputDateTimeArrival} IST`, {
+      ...data,
+      arrivalDate: `${outputDateTimeArrival} IST`,
+      departureDate: `${outputDateTimeDeparture} IST`,
+    });
     const response = await axios.post(
       "http://digitalaryasamaj.ap-south-1.elasticbeanstalk.com/event/register",
-      data,
+      {
+        ...data,
+        arrivalDate: `${outputDateTimeArrival} IST`,
+        departureDate: `${outputDateTimeDeparture} IST`,
+      },
       {
         headers: headers,
       }
     );
-    console.log("response.data.data :: ", response.data.data);
+
+    console.log("response.data.data :: ", response.data);
     return {
       status: statusCode.SUCCESS,
       data: response.data.data,
       messsage: Messages.ACTIVE_EVENTS,
     };
   } catch (error) {
-    console.log(error);
+    console.log("error :: ", error);
     return {
       status: statusCode.INTERNAL_SERVER_ERROR,
       data: error,
@@ -53,10 +80,29 @@ export const registeredEventUtils = async (headers: any) => {
         headers: headers,
       }
     );
-    console.log("response.data.data :: ", response.data.data);
+    const newData = response.data.data.map((E: any) => {
+      const utcDateTimeArrival = moment.utc(E.arrivalDate);
+      const utcDateTimeDeparture = moment.utc(E.departureDate);
+      const istDateTimeArrival = utcDateTimeArrival
+        .clone()
+        .subtract(5, "hours")
+        .format("YYYY-MM-DD HH:mm");
+      const istDateTimeDeparture = utcDateTimeDeparture
+        .clone()
+        .subtract(5, "hours")
+        .format("YYYY-MM-DD HH:mm");
+
+      E.arrivalDate = istDateTimeArrival;
+      E.departureDate = istDateTimeDeparture;
+      return E;
+    });
+    console.log(
+      " registered Event:: response.data.data :: ",
+      response.data.data
+    );
     return {
       status: statusCode.SUCCESS,
-      data: response.data.data,
+      data: newData,
       messsage: Messages.ACTIVE_EVENTS,
     };
   } catch (error) {
